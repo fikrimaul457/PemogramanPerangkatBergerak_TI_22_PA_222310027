c8a8726eeec7ef822148cc7fd3a893ac8525513a
fikrimaul457 <86140611+fikrimaul457@users.noreply.github.com>
2024-04-17 16:02:36 +0700

    Add files via upload

---
+++ Tugas_Pertemuan1/components/ MyProfile.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const MyProfile = () => {
+    return (
+        <View>
+            <Text>Nama:   Fikri Maulana</Text>
+            <Text>NPM:   222310027</Text>
+            <Text>Kelas:   TI-22-PA</Text>
+            <Text>Jurusan:   Teknologi Informasi</Text>
+            <Text>Kampus:   Institut Bisnis dan Informatika Kesatuan</Text>
+            <Text>Tanggal Lahir:   30 April 2004</Text>
+            <Text>Agama:   Islam</Text>
+            <Text>Alamat:   Cilendek Indah Green Garden Blok G No 4</Text>
+            <Text>No Telp/Whatsapp:   085695602553</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default MyProfile;
